577b4a6bbb7ed4888ff50480e761e0556f70d0d6
Conrado B V Ferreira <conradobvf@hotmail.com>
2022-10-16 15:59:06 -0300

    Finalização carrinho e checkout

--- src/components/cart/produtos-cart.js
+++ src/components/cart/produtos-cart.js
@@ -1,48 +1,43 @@
 const productsCart = {
-    33: {
-      name: "Produto 3",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
-      images: [
-        "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-        "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-        "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-      ],
-      price: 299.99,
-      categories: [1, 2, 3],
-      quantity: 3,
-    },
-    1: {
-      name: "Vestido laranja",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
-      images: [
-        "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-        "https://ph-cdn3.ecosweb.com.br/imagens01/foto/moda-feminina/vestido-midi/vestido-laranja-neon-com-babado-no-decote_337997_600_1.jpg",
-        "https://secretocloset.com.br/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/v/c/vc61.jpg",
-      ],
-      price: 299.99,
-      promo_price: 199.99,
-      percent: 15,
-      categories: [1, 2],
-      quantity: 2,
-    },
-    2: {
-      name: "Produto 2",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
-      images: [
-        "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-        "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-        "https://static3.tcdn.com.br/img/img_prod/769840/vestido_mirele_1329_1_20348177a5ec7e4dc844aae226aac5df.jpg",
-      ],
-      price: 299.99,
-      promo_price: 199.99,
-      percent: 15,
-      categories: [1, 3, 4],
-      quantity: 2,
-    },
-  };
+  33: {
+    name: "Pochete",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://i.pinimg.com/originals/c6/b5/00/c6b50012141eac6a909b099e39d3ade5.jpg",
+    ],
+    price: 59.90,
+    percent: '',
+    categories: [1, 2],
+    quantity: 3,
+  },
+  1: {
+    name: "Caneca",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSBqQy85dK_3G6KP3ytfvGQIWJ3r5MkrYOXseMbLyTc-xcItfWjpdyQaKlHtMaLDj_Bu2ONdgzcwmiiAQv5p_fIvIrdzcOKENc6pcogENXc&usqp=CAE",
+    ],
+    price: 39.90,
+    promo_price: 35.90,
+    percent: '10%',
+    categories: [3],
+    quantity: 3,
+  },
+  2: {
+    name: "Dundie Award",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dapibus lacus vel lectus dictum aliquam. In laoreet, arcu in porttitor semper, magna diam consectetur arcu, vel condimentum risus erat vel lacus. Duis ut viverra est, a facilisis augue. Integer feugiat risus risus. Curabitur facilisis ipsum at libero ullamcorper, id porta sapien rutrum. Integer egestas egestas quam, non venenatis quam lacinia sit amet. Ut massa metus, volutpat sit amet feugiat dapibus, tincidunt non est. Cras pharetra ut massa eu porta. Phasellus velit magna, pretium eleifend turpis in, dictum vehicula nunc.",
+    images: [
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSpPB57K1oAUog7UDVNKyYn5XyaBMG5bfFn0cd7uxgTr0fU1PjePrhYMuvooopkNjhUA32VjZrUN7KvYj6F5A_3qS5J4RspLvW1-n2jJn_rcITeh5f4uIkGnw",
+    ],
+    price: 299.99,
+    promo_price: 199.99,
+    percent: '15%',
+    categories: [5],
+    quantity: 3,
+  },
+};
   
   module.exports = {
     productsCart,
